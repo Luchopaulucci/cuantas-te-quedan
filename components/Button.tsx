d60c3f506d5295583a5624e01fe4ec30d60a5393
Joaquin Rodriguez <rodriguezcjoaquin02@gmail.com>
2025-02-12 23:32:36 -0300

    update ui

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ export const Button = ({ title, onClick }: BotonProps) => {
   return (
     <button
       onClick={onClick}
-      className="p-3 text-white bg-black font-semibold border-black border-solid border-x border-y sm:rounded-full rounded-xl  hover:scale-105 transition sm:w-full w-36"
+      className="p-3 text-white bg-black font-semibold border-black border-solid border-x border-y rounded-full   hover:scale-105 transition sm:w-full w-36"
     >
       {title}
     </button>
