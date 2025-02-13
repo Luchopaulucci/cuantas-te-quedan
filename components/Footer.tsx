c5ee48f50e0d3204632ff02596b87d0f239a633f
Joaquin Rodriguez <rodriguezcjoaquin02@gmail.com>
2025-02-12 23:12:55 -0300

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
-
 const Footer = () => {
   return (
-    <div className="h-6"></div>
-  )
-}
+    <div className="h-56 bg-black text-white gap-2 py-10 mt-10 flex flex-col text-2xl items-center justify-end">
+      <h1> Hecho por alumnos de la UTN de Mendoza. </h1>
+      <h1>📌 Mendoza, Argentina</h1>
+      <h2>{new Date().getFullYear()}</h2>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
