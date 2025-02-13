44d685aec2e62174748b53b0052c864651de8208
Joaquin Rodriguez <rodriguezcjoaquin02@gmail.com>
2025-02-12 23:46:39 -0300

    update progress bar

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -6,7 +6,7 @@ import { Progress } from "@/components/ui/progress";
 
 export function ProgressCustom({ progress }: { progress: number }) {
   return (
-    <div className="relative w-[60%]">
+    <div className="relative w-[90%]">
       <Progress value={progress} className="h-6" />
       <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-black">
         {Math.round(progress)}%
