70a8981e1cbdd4ace8be67bdad5ccc24ed1de2d4
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-04-09 08:00:55 -0300

    feat: add color and active attributes to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,32 @@
 type BotonProps = {
   title: string;
+  color?: string;
+  active: boolean;
   onClick: () => void;
   variant?: "destructive";
 };
 
-export const Button = ({ title, onClick }: BotonProps) => {
+export const Button = ({ title, color, onClick, active }: BotonProps) => {
+  const borderColors = {
+    yellow: `border-yellow-600 ${
+      active
+        ? "!bg-gradient-to-br from-yellow-500 to-yellow-600 text-white"
+        : "text-yellow-500 hover:bg-yellow-50"
+    }`,
+    green: `border-green-600 text-green-600 ${
+      active
+        ? "!bg-gradient-to-br from-green-500 to-green-600 text-white"
+        : "text-green-600 hover:bg-green-50"
+    }`,
+  };
   return (
     <button
       onClick={onClick}
-      className="p-3 text-white bg-black font-semibold border-black border-solid border-x border-y rounded-full   hover:scale-105 transition sm:w-full w-36"
+      className={`px-4 bg-gradient py-2 border bg-white ${
+        color
+          ? borderColors[color as keyof typeof borderColors]
+          : "border-gray-400"
+      } font-semibold !text-xs rounded-lg transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400`}
     >
       {title}
     </button>
